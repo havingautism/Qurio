075b5770e044ae77fa681cc8c1ecfe10c03ee84b
havingautism <havingautism@outlook.com>
2025-12-20 22:29:15 +0800

    feat: Set up `_app.jsx` to manage app-wide layout, SEO, and global styles.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,22 @@
 import Head from 'next/head'
 import '../src/index.css'
 
+const getBasePath = () => {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+  return basePath.replace(/\/$/, '')
+}
+
 export default function App({ Component, pageProps }) {
+  const basePath = getBasePath()
   return (
     <>
       <Head>
         <title>Qurio</title>
         <meta name="description" content="Qurio" />
-        <link rel="icon" href="/Qurio-logo-app-webtab.png" />
-        <link rel="apple-touch-icon" href="/Qurio-logo-app.png" />
+        <meta name="theme-color" content="#18181b" />
+        <link rel="manifest" href={`${basePath}/manifest.json`} />
+        <link rel="icon" href={`${basePath}/Qurio-logo-app-webtab.png`} />
+        <link rel="apple-touch-icon" href={`${basePath}/Qurio-logo-app.png`} />
       </Head>
       <Component {...pageProps} />
     </>
